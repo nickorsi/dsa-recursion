162c88db5163da2083edbb58400775e00543614c
Nicholas <nick.orsi1992@gmail.com>
2024-02-27 12:19:43 -0800

    Added functionality to the function everyOther.

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,8 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  if(str.length === 0) return ""
+  return str[0] + everyOther(str.slice(2))
 }
 
 export { everyOther };
