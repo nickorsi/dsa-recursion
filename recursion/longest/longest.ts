509946aad3cd3285e23507e6c222692a981243fb
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 13:37:30 -0700

    implemented Longest
    - all tests passing

--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -3,7 +3,11 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  // base case -> words: is null?
+  if (!words) return 0;
+
+  const currWordLength = words.val.length;
+  return Math.max(currWordLength, longest(words.next));
 }
 
 export { longest };
