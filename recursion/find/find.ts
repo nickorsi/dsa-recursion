21386128165149628c9acb8e62336099fc1d8657
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 15:51:18 -0700

    solved find.ts
    - tests passing

--- recursion/find/find.ts
+++ recursion/find/find.ts
@@ -1,7 +1,11 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  // base case -> arr is empty or val is found
+  if (arr.length === 0) return false;
+  if (arr[0] === val) return true;
+
+  return find(arr.slice(1), val);
 }
 
 export { find };
