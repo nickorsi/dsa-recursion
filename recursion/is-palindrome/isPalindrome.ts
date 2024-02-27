407288c84d14d15d9c71f49415c5a2a7d470f88c
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 15:58:33 -0700

    isPalindrome complete
    - tests passing

--- recursion/is-palindrome/isPalindrome.ts
+++ recursion/is-palindrome/isPalindrome.ts
@@ -1,7 +1,13 @@
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str: string): boolean {
-  return true;
+  // base case(s)
+  // get to center so str.length === 1 -> TRUE
+  if (str.length < 2) return true;
+  // the first value and last value are not equal -> FALSE
+  if (str[0] !== str[str.length - 1]) return false;
+
+  return isPalindrome(str.slice(1, str.length - 1));
 }
 
 export { isPalindrome };
