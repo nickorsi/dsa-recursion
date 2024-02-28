3ff11eaac96fe9db10f886bbb9924bdb2d6b23f4
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 17:18:58 -0700

    added non-recursive binary search

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -6,3 +6,37 @@ function binarySearch(arr: number[], val: number, left = 0, right = arr.length):
 }
 
 export { binarySearch };
+
+
+// Non-recursive binary search
+
+function nonRecBinarySearch(arr: number[], item: number, left = 0, right = arr.length): boolean {
+  let low = left;
+  let high = arr[right - 1];
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    console.log(mid);
+    let guess = arr[mid];
+    console.log(guess);
+
+    if (guess === item) {
+      return true;
+    }
+
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return false;
+}
+
+const myList = [1, 3, 5, 7, 9];
+
+console.log(nonRecBinarySearch(myList, 1)); // true
+console.log(nonRecBinarySearch(myList, 2)); // false
+console.log(nonRecBinarySearch(myList, 6)); // false
+console.log(nonRecBinarySearch(myList, 9)); // true
