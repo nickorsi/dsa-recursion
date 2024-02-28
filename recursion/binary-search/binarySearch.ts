025689f0cc4c4d1b9fbaf6110f8269e2f62ce856
Nathan Rydel <nathanrydel@gmail.com>
2024-02-27 18:13:35 -0700

    completed binarySearch
    - all tests passing

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -2,21 +2,37 @@
  * return true if val is in array, false if not present). */
 
 function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+  const mid = Math.floor((left + right) / 2);
+
+  // base case(s):
+  // if arr[mid] === val -> true
+  if (arr[mid] === val) return true;
+  // if left > right -> false
+  if (left > right) return false;
+  // if arr[mid] > val -> binarySearch(arr, val, left, right= mid - 1)
+  if (arr[mid] > val) {
+    return binarySearch(arr, val, left, right = mid - 1);
+  } else {
+    // if arr[mid] < val -> binarySearch(arr, val, left= mid + 1, right)
+    return binarySearch(arr, val, left = mid + 1, right);
+  }
 }
 
 export { binarySearch };
 
+// console.log(binarySearch([1, 2, 3, 4], 4));
 
+// console.log(binarySearch([], 0)); // false
+// console.log(binarySearch([1, 2, 3, 4], 0)); // false
+// console.log(binarySearch([1, 2, 3, 4], 11)); // false
 // Non-recursive binary search
 
 function nonRecBinarySearch(arr: number[], item: number, left = 0, right = arr.length): boolean {
   let low = left;
-  let high = arr[right - 1];
+  let high = right;
 
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
-    console.log(mid);
     let guess = arr[mid];
     console.log(guess);
 
@@ -34,9 +50,9 @@ function nonRecBinarySearch(arr: number[], item: number, left = 0, right = arr.l
   return false;
 }
 
-const myList = [1, 3, 5, 7, 9];
+// const myList = [1, 3, 5, 7, 9];
 
-console.log(nonRecBinarySearch(myList, 1)); // true
-console.log(nonRecBinarySearch(myList, 2)); // false
-console.log(nonRecBinarySearch(myList, 6)); // false
-console.log(nonRecBinarySearch(myList, 9)); // true
+// console.log(nonRecBinarySearch(myList, 1)); // true
+// console.log(nonRecBinarySearch(myList, 2)); // false
+// console.log(nonRecBinarySearch(myList, 6)); // false
+// console.log(nonRecBinarySearch(myList, 9)); // true
